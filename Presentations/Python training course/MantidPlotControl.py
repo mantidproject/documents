8dcad7d080ee5cc7881f274ea077c00f78d1f29c
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2009-11-27 14:40:44 +0000

    Updated slides for both Python training courses. Re #100

--- Presentations/Python training course/MantidPlotControl.py
+++ Presentations/Python training course/MantidPlotControl.py
@@ -3,7 +3,7 @@
 #  Controlling the MantidPlot items
 #------------------------------------------------------
 # Perform some algorithms to so that we have some sensible data to plot and look at
-rootdir = "C:/Mantid/Test/data/"
+rootdir = "C:/MantidInstall/data/"
 # Load bank 1 from GEM
 LoadRaw(rootdir+ "GEM40979.raw", "GEM-bank2", SpectrumMin="431", SpectrumMax="750")
 
@@ -13,12 +13,8 @@ ConvertUnits("GEM-bank2","GEM-bank2", "dSpacing")
 # Smooth the data
 SmoothData("GEM-bank2", "GEM-bank2", NPoints="20")
 
-# Plot two spectra and merge
-g1 = plotSpectrum("GEM-bank2",0)
-mergePlots(g1, plotSpectrum("GEM-bank2",1))
-
-# Add another spectra
-mergePlots(g1, plotSpectrum("GEM-bank2",2))
+# Plot three spectra
+g1 = plotSpectrum("GEM-bank2",[0,1,2])
 
 # Set the scales on the x- and y-axes
 layer = g1.activeLayer()
@@ -29,3 +25,5 @@ layer.setAxisScale(Layer.Left, 0, 15)
 layer.setCurveTitle(0, "bank2, spectrum " + str(431))
 layer.setCurveTitle(1, "bank2, spectrum " + str(432))
 layer.setCurveTitle(2, "bank2 spectrum "+ str(433))
+
+plotSpectrum(mtd.getMatrixWorkspace('GEM-bank2'),[0])
