b7ea61edd7dcd0b41ca12c07ad1b90e361a21e72
Stuart Campbell <campbellsi@ornl.gov>
2011-09-14 15:40:38 +0000

    Exercise 3a solution for SNS data.

---
+++ Presentations/Python training course/MantidPlotControl_SNS.py
@@ -0,0 +1,21 @@
+# -----------------------------------------------------
+#  Python Training Exercise 3 Solution.
+#  Controlling the MantidPlot items
+#------------------------------------------------------
+# Perform some algorithms to so that we have some sensible data to plot and look at
+rootdir = "C:/MantidInstall/data/"
+# Load processed data from CNCS
+LoadNexusProcessed(rootdir+ "Training_Exercise3a_SNS.nxs", "CNCS")
+
+# Plot three spectra
+g1 = plotSpectrum("CNCS",[0,1,2])
+
+# Set the scales on the x- and y-axes
+layer = g1.activeLayer()
+layer.setAxisScale(Layer.Bottom, -1.5, 1.8)
+layer.setAxisScale(Layer.Left, 1, 2500, Layer.Log10)
+
+# Optionally rename the curve titles
+layer.setCurveTitle(0, "CNCS, Q = 0.3")
+layer.setCurveTitle(1, "CNCS, Q = 0.5")
+layer.setCurveTitle(2, "CNCS, Q = 0.7")
