9eea20320ced53053a74f5b14acdbd5c79ae244a
Antti Soininen <soininen@ill.fr>
2017-06-22 16:26:03 +0200

    Add Python script to access a random file in ILL's shared data folders.

---
+++ Project-Management/ILL/random_nxs_file.py
@@ -0,0 +1,23 @@
+import glob
+import os.path
+import random
+
+def randomFile(instrument, years, basePath=os.path.join('/' 'net', 'serdon', 'illdata')):
+    """Return a path to a random file in ILL's shared data folders.
+
+       Parameters:
+       instrument - case insensitive name of the instrument, such as in4, figaro...
+       years - an iterable containing the last two digits of the years to include.
+       basePath - a path as a string pointing to the shared 'illdata' folder.
+    """
+    instrument = instrument.lower()
+    year = random.choice(years)
+    yearDirs = glob.glob(os.path.join(basePath, '{:2}?'.format(year)))
+    yearDir = random.choice(yearDirs)
+    instrDir = os.path.join(yearDir, instrument)
+    expDirs = glob.glob(os.path.join(instrDir, 'exp_*'))
+    expDir = random.choice(expDirs)
+    dataDir = os.path.join(expDir, 'rawdata')
+    files = glob.glob(os.path.join(dataDir, '*.nxs'))
+    return random.choice(files)
+
