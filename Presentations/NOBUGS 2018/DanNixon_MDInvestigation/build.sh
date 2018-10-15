aa9bb2cd4c6505bcf2976f0a9a820d4e98f8b8e4
Dan Nixon <dan@dan-nixon.com>
2018-10-15 12:41:25 +0100

    Slides from NOBUGS MDEventWorkspace presentation

---
+++ Presentations/NOBUGS 2018/DanNixon_MDInvestigation/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Builds stuff that needs to be built
+
+inkscape -z -e box_splitting.png -w 1200 -h 550 box_splitting.svg
+inkscape -z -e box_event_filling.png -w 1200 -h 200 box_event_filling.svg
+inkscape -z -e z_order_curve.png -w 1200 -h 1200 z_order_curve.svg
