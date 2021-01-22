61deeb27b36d11f3d57ad4d645e4d47f936b664d
Daniel Murphy <daniel.murphy@stfc.ac.uk>
2021-01-22 16:56:06 +0000

    Update scripts to create Manual and Smoke testing issues
    Manual tests issue_template has had Mantidplot and Vates removed, Sliceviewer added.
    ISIS related manual tests were split out to issue_template_ISIS_11_2020.xlsx
    
    SmokeTesting issue_template has had some minor updates, mostly related to plotting
    create_issues_OS.py has separated the Smoke testing issues by OS
    
    READMEs have been updates to reflect these changes AND the move to Personal Access Tokens
    https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token

--- Project-Management/Tools/RoadmapUpdate/SmokeTesting/README.md
+++ Project-Management/Tools/RoadmapUpdate/SmokeTesting/README.md
@@ -1,2 +1,23 @@
 # SmokeTests
 To create and update a script for Smoke Test issue creation
+
+- In `issue_template.xlsx` update any links or text for each testing issue.
+
+- Create a Personal Access Token for PyGithub: https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token This token then is used in place of a password
+- Create a local file in the RoadmapUpdate directory called `login.txt` simply with your Github 
+Username and PAToken on seperate lines. e.g.
+```
+abc12345
+ff34885a86faketoken24460a8555...
+```
+ **Do not commit login.txt to the remote repo!**
+
+-`create_issues.py` creates an umbrella issue with 6 sub-issues describing each test.
+-`create_issues_OS.py` does something similar, but creates separate issues for each OS to better track assignment.
+- Firstly, comment out the two `repo.create()` lines in either create_issues script to test the script runs successsfully, assigning Manual Testing to recognised Mantid developers on GitHub:
+```
+# issue = repo.create_issue(title, my_body, gh_assignee, gh_milestone, gh_labels)
+```
+
+- Then uncomment to run and create the issues!
+- Check your handiwork!
--- Project-Management/Tools/RoadmapUpdate/create_issues.py
+++ Project-Management/Tools/RoadmapUpdate/create_issues.py
@@ -41,7 +41,7 @@ repo = gh.get_user(REPO_OWNER).get_repo(REPO_NAME)
 assignees = repo.get_assignees()
 
 #Lookup milestone sting to get number
-milestone = "Release 5.1"
+milestone = "Release 6.0 S3"
 gh_milestone = None
 for loop_milestone in repo.get_milestones():
     if loop_milestone.title == milestone:
@@ -63,9 +63,9 @@ print( "Labels", gh_labels)
 
 
 body_text = '''
-You have been assigned unscripted testing. The hope is to catch as many problems with the code before release, so it would be great if you can take some time to give a serious test to your assigned area. Thank you!!
+You have been assigned manual testing. The hope is to catch as many problems with the code before release, so it would be great if you can take some time to give a serious test to your assigned area. Thank you!!
 
-The general guide to unscripted testing:
+The general guide to manual testing:
 
 * The tests must be performed on the installer versions of the final release candidate. Not on local compiled code.
 * Serious errors involving loss of functionality, crashes etc. should be raised
@@ -80,12 +80,13 @@ releases.
 '''
 
 print( "\nLoading Issues")
-df = pd.read_excel("issue_template.xlsx","issues")
+df = pd.read_excel("issue_template_01_2021.xlsx","issues")
 
 print( "\nCreating Issues")
 for index, row in df.iterrows():
     title =  row['Title']
     additional_body = row['Additional Body Text'] 
+    gh_labels = [repo.get_label('Manual Tests')]
     assignee =  row['Assignee']
     gh_assignee = None
     if pd.notnull(assignee):
@@ -98,6 +99,6 @@ for index, row in df.iterrows():
     my_body = body_text
     if pd.notnull(additional_body):
         my_body += "\n\n### Specific Notes:\n\n" + additional_body
-    print (title,gh_assignee,gh_milestone,gh_labels)
-    issue = repo.create_issue(title, my_body, gh_assignee, gh_milestone, gh_labels) #COMMENT THIS OUT TO TEST BEFORE MAKING ISSUES
+    print (title,None,gh_milestone,gh_labels,gh_assignee)
+    issue = repo.create_issue(title, body=my_body, milestone=gh_milestone, labels=gh_labels, assignee=gh_assignee) #COMMENT THIS OUT TO TEST BEFORE MAKING ISSUES
     print(issue.number, issue.title)
